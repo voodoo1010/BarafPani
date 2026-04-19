1a9011eb6700231b6e7d582c4b9d036d889e5f01
Twani <zainulabaidin16@gmail.com>
2026-04-19 10:50:33 +0500

    Camera Input Desync Fixed

--- Assets/_Features/Player/_Features/CameraView/_Features/FirstPerson/Config/Scripts/CharacterFirstPersonCameraSettings.cs
+++ Assets/_Features/Player/_Features/CameraView/_Features/FirstPerson/Config/Scripts/CharacterFirstPersonCameraSettings.cs
@@ -9,7 +9,7 @@ namespace _Features.Player._Features.CameraView._Features.FirstPerson.Config.Scr
         public float PitchClampMin = -80f;
         [Tooltip("Maximum pitch angle (looking up)"), Range(0f, 90f)]
         public float PitchClampMax = 80f;
-        [Tooltip("Camera height from character origin in units")]
-        public Vector3 EyeHeight;
+        [Tooltip("Camera offset from character origin (X = side, Y = height, Z = forward)")]
+        public Vector3 EyeHeight = new Vector3(0f, 1.7f, 0f);
     }
 }
--- Assets/_Features/Player/_Features/CameraView/_Features/FirstPerson/Scripts/CharacterFirstPersonCamera.cs
+++ Assets/_Features/Player/_Features/CameraView/_Features/FirstPerson/Scripts/CharacterFirstPersonCamera.cs
@@ -1,4 +1,4 @@
-using _Features.Player._Features.CameraView._Features.FirstPerson.Config.Scripts;
+﻿using _Features.Player._Features.CameraView._Features.FirstPerson.Config.Scripts;
 using _Features.Player._Features.CameraView.Scripts;
 using CustomInspector;
 using Unity.Cinemachine;
@@ -17,15 +17,22 @@ namespace _Features.Player._Features.CameraView._Features.FirstPerson.Scripts
         {
             base.Awake();
 
-            CinemachineCamera.Follow = Character.transform;
+            var eyePivot = new GameObject("[EyePivot]").transform;
+            eyePivot.SetParent(Character.transform);
+            eyePivot.localPosition = firstPersonSettings.EyeHeight; // full Vector3, set by you
+            eyePivot.localRotation = Quaternion.identity;
+
+            CinemachineCamera.Follow = eyePivot;
 
             var follow = CinemachineCamera.GetComponent<CinemachineFollow>();
-            follow.FollowOffset = firstPersonSettings.EyeHeight;
+            if (follow != null)
+                DestroyImmediate(follow); // immediate so AddComponent below doesn't conflict
+
+            CinemachineCamera.gameObject.AddComponent<CinemachineHardLockToTarget>();
 
             _panTilt = CinemachineCamera.GetComponent<CinemachinePanTilt>();
             _panTilt.ReferenceFrame = CinemachinePanTilt.ReferenceFrames.World;
         }
-
         protected override void ApplyLook(float yaw, float pitch)
         {
             _panTilt.PanAxis.Value += yaw;
@@ -35,6 +42,7 @@ namespace _Features.Player._Features.CameraView._Features.FirstPerson.Scripts
                 firstPersonSettings.PitchClampMax
             );
 
+            // Body rotates yaw-only; camera pitch is handled entirely by PanTilt
             Character.transform.rotation = Quaternion.Euler(0f, _panTilt.PanAxis.Value, 0f);
         }
     }
